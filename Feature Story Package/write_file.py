ea617c6f027d9422ecc5d472919267304c7da408
Dhruval Patel <dhruvalpatel333@gmail.com>
2021-09-28 21:30:27 -0600

    Update write_file.py

--- Feature Story Package/write_file.py
+++ Feature Story Package/write_file.py
@@ -6,22 +6,18 @@ import shutil,os
 import os.path
 import sys, codecs
 
-
-# Feature =r'C:\Users\Admin\Documents\WORK\Feature_story\Feature.html'
-# url = 'https://www.uregina.ca/external/communications/feature-stories/current/2019/01-07.html'
-# r = ureq(url)
-
 # Change the year as per the story index required (Apply the change to both baseurl as well as r)
-baseurl = "https://www.uregina.ca/external/communications/feature-stories/current/2020/"
-r = ureq('https://www.uregina.ca/external/communications/feature-stories/current/2020/index.html')
+baseurl = "https://www.uregina.ca/external/communications/feature-stories/current/2019/"
+r = ureq('https://www.uregina.ca/external/communications/feature-stories/current/2019/index.html')
 soup = BeautifulSoup(r,"html5lib")
-# counter = 1
+
 storylinks = []
 store_count = {}
 for title in soup.find_all('td', class_='title'):
     for link in title.find_all('a', href=True):
         storylinks.append(baseurl + link['href'])
-
+        
+# Featching data from site using soup
 for link in storylinks:
     r = ureq(link)
     soup = BeautifulSoup(r,"html5lib")
@@ -30,7 +26,7 @@ for link in storylinks:
     month = datetime.strptime(date,'%Y-%m-%d').strftime('%m-%B')
     year = datetime.strptime(date,'%Y-%m-%d').strftime('%Y')
     day = datetime.strptime(date,'%Y-%m-%d').strftime("%d")
-    lin= soup.find('div',class_='globalCol_1_1')
+    lin = soup.find('div',class_='globalCol_1_1')
     link_new = lin.findAll('img')
     body = soup.find('div',id='dspace')
     # read file
@@ -91,6 +87,7 @@ for link in storylinks:
     if not os.path.exists(m):
         os.makedirs(m)
 
+    #Bypass pages
     if (month == '08-August' and day == '30' or day == '13'): 
       continue
     # if (month == '08-August' and day == '22' or day == '17'or day == '11' or day == '08' or day == '07' or day == '04'): 
@@ -121,9 +118,13 @@ for link in storylinks:
       filepath = os.path.join(src_file,f)
       shutil.copy(filepath,folder)
 
+    
+    #Image download process
     for link_new in lin.findAll('img',src=True):
       story = baseurl + link_new['src']
       name = link_new['src']
+       
+      #Bypass dead link and image
       if story == 'https://www.uregina.ca/external/communications/feature-stories/current/2019/https://cascade.uregina.ca:8443/render/file.act?path=feature-stories/current/2019/images/theFalls-3.png':
         continue
       if name == 'https://counter.theconversation.com/content/151769/count.gif?distributor=republish-lightbox-advanced':
@@ -135,6 +136,8 @@ for link in storylinks:
       print('Writing:',name)
       f.close()
     
+    
+    #Image naming
     for a in body.find_all('img',src=True):
       a['src'] = a['src'].replace('images/','').replace('../2018/','').replace('../','').replace('..','')
       a['src']  = './' + a['src']
